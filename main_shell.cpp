a30ac0de7678be8adfe769720d5e852e7e77c7c8
chauhan-manish <chauhanmanish1258@gmail.com>
2019-09-04 00:01:00 +0530

    Second Commit

--- main_shell.cpp
+++ main_shell.cpp
@@ -21,7 +21,6 @@ string prompt()
 		{
 			perror(args[0]);
 		}
-		
 	}
 	else
 	{
@@ -39,54 +38,147 @@ string prompt()
 	return s1;
 }
 
-void split(string command, char* args[])
+long split(string command, vector<string> &args, char *delim)
 {
-	const char* delim=" ";
 	char* ptr = strtok( (char*)command.c_str(), delim);
 	long i=0;
 	while(ptr != NULL)
 	{
+		
+		args[i] = string(ptr);
+		//cout<<args[i]<<"\n";
+		ptr = strtok(NULL, delim);
+		i++;
+	}
+	args[i]="\0";
+	return i;
+}
+long split1(string command, char *args[100], char *delim)
+{
+	char* ptr = strtok( (char*)command.c_str(), delim);
+	long i=0;
+	while(ptr != NULL)
+	{
+		
 		args[i] = ptr;
 		//cout<<string(args[i])<<"\n";
 		ptr = strtok(NULL, delim);
 		i++;
 	}
 	args[i]=NULL;
+	return i;
 }
-
 int main()
 {
-	char *args[100],*ptr, path[100];
-	string ps1 = getenv("USER"), command;
-	long i,x=5;
-	ps1 = ps1 + "@" + prompt() + ": ";
-	ps1 = ps1 + getcwd(path,100) + "$ ";
+	int fd;
+	char* delim;
+	char *args[100];
+	vector< string >str(100);
+	pid_t pid;
+	string ps1, tmp = getenv("USER"), command, filename="temp.txt";
+	long i, n, x;
+	tmp = tmp + "@" + prompt() + ": ";
 	//cout<< ps1;
-	while(x--)
+	while(1)
 	{
-		//path = getcwd(path,100);
+		char path[100]={'\0'};
+		ps1="";
+		ps1 = tmp + getcwd(path,100) + "$ ";
 		cout << ps1;
 		getline( cin, command );
-		split(command, args);
-		pid_t pid=fork();
 		
-		if( pid==0 )
+		delim = "|";
+		n = split(command, str, delim);
+		if( n > 1 )
 		{
-			/*int fd = open("temp.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			close(1);
-			dup(fd);
-			close(fd);
-			*/
-			if( execvp(args[0], args) == -1)
+			//cout<<n<<"\n";
+			delim = " ";
+			x = split1(str[0], args, delim);
+			pid=fork();
+			
+			if( pid==0 )
 			{
-				perror(args[0]);
+				fd = open("temp.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				close(1);
+				dup(fd);
+				close(fd);
+	
+				if( execvp(args[0], args) == -1)
+				{
+					perror(args[0]);
+				}
+			}
+			else
+			{
+				wait(0);
 			}
 			
+			for(i=1;i<n-1;i++)
+			{
+				x = split1(str[i], args, delim);
+				args[x++]=(char*)filename.c_str();
+				args[x]=NULL;
+				pid=fork();
+				if( pid==0 )
+				{
+					fd = open("temp.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					close(1);
+					dup(fd);
+					close(fd);
+		
+					if( execvp(args[0], args) == -1)
+					{
+						perror(args[0]);
+					}
+				}
+				else
+				{
+					wait(0);
+				}
+			}
+			
+			filename="temp.txt";
+			x = split1(str[1], args, delim);
+			args[x++]=(char*)filename.c_str();
+			args[x]=NULL;
+			pid=fork();
+			if( pid==0 )
+			{
+				if( execvp(args[0], args) == -1)
+				{
+					perror(args[0]);
+				}
+			}
+			else
+			{
+				wait(0);
+			}
 		}
 		else
 		{
-			wait(0);
+			delim = " ";
+			split1(command, args, delim);
+			if(string(args[0])=="cd")
+			{
+				chdir(args[1]);
+			}
+			else
+			{
+				pid=fork();
+				if( pid==0 )
+				{			
+					if( execvp(args[0], args) == -1)
+					{
+						perror(args[0]);
+					}
+				}
+				else
+				{
+					wait(0);
+				}
+			}
 		}
+		
 	}
 	return 0;
 }
